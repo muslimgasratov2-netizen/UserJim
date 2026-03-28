5e6a8682e4ebd65ce4068ed0b6af339544dca67d
Muslim <muslimgasratov2@gmail.com>
2026-03-28 13:18:59 +0300

    Начал новый проект

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using Project;
+
+var muslim = new User("muslimchek", "muslimchek@gmail.com","0000000");
+var ggez = new User();
---
+++ User.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel;
+
+namespace Project;
+
+class User
+{
+    public string  UserName { get; set; } = string.Empty;
+
+    public string  Password { get; set; } = string.Empty;
+
+
+    public string  Email { get; set; } = string.Empty;
+
+    public User(string name, string email, string password)
+    {
+        UserName = name;
+        Email = email;
+        Password = password;
+        System.Console.WriteLine($"{UserName} создан");
+        
+    }
+
+    public User() : this("Данил Колбасенко", "danyagig@gmail.com", "12345678") {}
+
+
+
+
+
+}
+
+
+
